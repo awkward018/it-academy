9abf1c23104a759b303b13fa8541008df3488ac0
саня высочина <2001sashka@tut.by>
2019-03-18 21:27:55 +0300

    commit

---
+++ hm-04/hm-04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter;
+            for (letter = '\u005A'; letter>='\u0041'; letter--)
+            { Console.WriteLine(letter); }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ hm-04/hm04(2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm04_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter symbol");
+            char symbol;
+            symbol = char.Parse(Console.ReadLine());
+            
+            switch(symbol)
+            {
+                case char x when (symbol=='D') :
+                    Console.WriteLine("Turn right");
+                    break;
+                case char x when (symbol == 'A'):
+                    Console.WriteLine("Turn left");
+                    break;
+                case char x when (symbol == 'W'):
+                    Console.WriteLine("Go up");
+                    break;
+                case char x when (symbol == 'S'):
+                    Console.WriteLine("Go down");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
