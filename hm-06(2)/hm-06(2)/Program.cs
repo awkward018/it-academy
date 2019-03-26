35ea82af965c6033f817d9c7f8f4ffe720804f4e
саня высочина <2001sashka@tut.by>
2019-03-26 22:37:25 +0300

    first

---
+++ hm-06(2)/hm-06(2)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace hm_06_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line, separate words with blank space");
+            string[] array = Console.ReadLine().Split(' ');
+            int a=0;
+            
+            string max = array.Max();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == max)
+                    a = i;  
+             }
+          
+            for (int i = 0; i < array.Length; i++)
+            { if (i != a)
+                    Console.WriteLine(array[i]);
+           }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
