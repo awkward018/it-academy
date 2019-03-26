cf8c98604bfbf4bda90b4398c0138bb9b48c9ab7
саня высочина <2001sashka@tut.by>
2019-03-26 20:39:46 +0300

    commit

---
+++ hm-06/hm-06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hm_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Enter poem, separating lines with \";\"");
+                string s1 = Console.ReadLine();
+                string s2 = s1.Replace("o", "a");
+                string[] array = s2.Split(";");
+
+                for (int i = 0; i < array.Length; i++)
+                    Console.WriteLine(array[i]);
+
+                Console.ReadLine();
+
+            
+        }
+    }
+}
+    
+
