1e84a658abfc2edae464327c31b8b7736ddaa285
саня высочина <2001sashka@tut.by>
2019-03-27 22:01:41 +0300

    commit

---
+++ hm-06(2).4/hm-06(2).4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace hm_06_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line, separate words with blank space");
+            string[] array = Console.ReadLine().Split(' ');
+
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            Console.WriteLine("Editted array:");
+
+            for (int i = 0; i < array.Length; i++) 
+            Console.WriteLine(array[i]);
+            
+            Console.ReadLine();
+        }
+    }
+}
+
