96212f7472e007449b7cab2daf31a5a0a4ac67c2
саня высочина <2001sashka@tut.by>
2019-04-02 16:43:00 +0300

    commit

---
+++ hm_07/AssemblyTwo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    
+    class Developer: Employee
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Employee.ProtectedCompany);
+            Console.WriteLine(Employee.PublicCompany);
+            Console.ReadLine();
+            
+       
+        }
+
+    }
+}
+
